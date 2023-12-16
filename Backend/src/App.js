6e13c742ec681ca792ead1cf44bee498aaf86899
wh1sky <yvonnedo2809@gmail.com>
2023-12-16 18:08:49 +0700

    Create Sidebar

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import gptLogo from './asset/chatgpt.svg';
+import addBtn from './asset/add-30.png';
+import msgIcon from './asset/message.svg';
+import home from './asset/home.svg';
+import saved from './asset/bookmark.svg';
+import rocket from './asset/rocket.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop"><img src={gptLogo} alt="Logo" className="logo"/><span className="brand">ChatGPT</span></div>
+          <button className="midBtn"><img src={addBtn} alt="New Chat" className="addBtn"/>New Chat</button>
+          <div className="upperSideBottom">
+            <button className="query"><img src={msgIcon} alt="Query"/>What is Programming?</button>
+            <button className="query"><img src={msgIcon} alt="Query"/>How to use an API?</button>
+          </div>
+        </div>
+          <div className="lowerSide">
+              <div className="listItems"><img src={home} alt="Home" className="listItemsImg"/>Home</div>
+              <div className="listItems"><img src={saved} alt="Saved" className="listItemsImg"/>Saved</div>
+              <div className="listItems"><img src={rocket} alt="Rocket" className="listItemsImg"/>Upgrade to Pro</div>
+          </div>
+      </div>
+        <div className="main">
+
+        </div>
     </div>
   );
 }
